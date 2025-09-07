1b5162324bc25a094eca3f93d99ba8525d62c745
shadowmoonezios <agushifelde@hotmail.com>
2025-09-07 22:32:50 +0300

    feat: initial commit for server.js

---
+++ server.js
@@ -0,0 +1,18 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const bodyParser = require('body-parser');
+const cors = require('cors');
+
+const app = express();
+app.use(bodyParser.json());
+app.use(cors());
+
+const PORT = process.env.PORT || 5000;
+
+mongoose.connect('mongodb://localhost/event-planer', { useNewUrlParser: true, useUnifiedTopology: true })
+  .then(() => console.log('MongoDB verbunden...'))
+  .catch(err => console.log(err));
+
+app.listen(PORT, () => {
+  console.log(`Server läuft auf Port ${PORT}`);
+});
